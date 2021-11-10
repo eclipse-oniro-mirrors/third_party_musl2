8c8015667202bc0faf6ab2ba045908fbf67cfbc1
caifuzhou <504631861@qq.com>
2021-11-10 09:20:57 +0800

    修改脚本runtest.sh runtest.bat以及 tls_align_dlopen.c和tls_init_dlopen.c的动态库路径
    Signed-off-by: caifuzhou <504631861@qq.com>

---
+++ scripts/runtest.sh
@@ -0,0 +1,17 @@
+cd /data/tests/libc-test/src
+rm REPORT 
+touch REPORT 
+for filelist in $(ls)
+do
+    cd /data/tests/libc-test/src/$filelist
+    for file in $(ls)
+    do
+        if [ "$file" = "runtest" ] || [ "$file" = "libdlopen_dso.so" ] || [ "$file" = "libtls_align_dso.so" ] || [ "$file" = "libtls_get_new-dtv_dso.so" ] || [ "$file" = "libtls_init_dso.so" ]; then 
+            continue
+        else
+            /data/tests/libc-test/src/common/runtest -w '' $file >>../REPORT 
+            echo $filelist/$file
+        fi
+    done
+    cd ..
+done
